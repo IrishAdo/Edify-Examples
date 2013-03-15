d7d97e7cf95b234271a831e8abab49966bae247e
IrishAdo <irishado@php-programmers.net>
2013-03-15 23:21:43 +0000

    Fixed path for drivers as they are not held in a subdirectory

--- Cache/Example1/htdocs/index-without.php
+++ Cache/Example1/htdocs/index-without.php
@@ -5,8 +5,8 @@
 define("PP_NET_CL_PATH", realpath("../../../../Edify"));
 require_once PP_NET_CL_PATH . "/Utils/Log.php";
 require_once PP_NET_CL_PATH . "/Cache/Factory.php";
-require_once PP_NET_CL_PATH . "/Cache/Drivers/Dynamic.php";
-require_once PP_NET_CL_PATH . "/Cache/Drivers/Statics.php";
+require_once PP_NET_CL_PATH . "/Cache/Dynamic.php";
+require_once PP_NET_CL_PATH . "/Cache/Statics.php";
 
 require_once 'example1.php';
 ?>
